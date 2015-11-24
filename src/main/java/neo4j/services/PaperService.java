51e29628326a09f901e3e9227d986e83aa1736c8
Chia-Chuan Wu <chia.chuan.wu@sv.cmu.edu>
2015-11-23 22:57:07 -0800

    add some addition labels

--- src/main/java/neo4j/services/PaperService.java
+++ src/main/java/neo4j/services/PaperService.java
@@ -25,18 +25,18 @@ public class PaperService {
             i++;
             target = i;
             for (Object name : (Collection) row.get("cast")) {
-                Map<String, Object> author = map("name", name,"label","author");
+                Map<String, Object> author = map("name", name, "label", "author");
                 int source = nodes.indexOf(author);
                 if (source == -1) {
                     nodes.add(author);
                     source = i++;
                 }
-                rels.add(map("source",source,"target",target));
+                rels.add(map("source", source, "target", target));
             }
         }
         return map("nodes", nodes, "links", rels);
     }
-    
+
     private Map<String, Object> toAlcFormat(Iterator<Map<String, Object>> result) {
         List<Map<String,Object>> nodes = new ArrayList<Map<String, Object>>();
         List<Map<String,Object>> rels = new ArrayList<Map<String, Object>>();
@@ -44,17 +44,16 @@ public class PaperService {
         int target = 0;
         while (result.hasNext()) {
             Map<String, Object> row = result.next();
-            nodes.add(map6("id", i, "title",row.get("paper"),"label", "paper", "cluster", "1", "value", 2, "group", "paper"));
+            nodes.add(map6("id", i, "title", row.get("paper"), "label", row.get("paper"), "cluster", "1", "value", 2, "group", "paper"));
             target = i++;
             for (Object name : (Collection) row.get("cast")) {
-                Map<String, Object> author = map5("title",
-                        name, "label", "author", "cluster", "2", "value", 1, "group", "author");
+                Map<String, Object> author = map5("title", name, "label", name, "cluster", "2", "value", 1, "group", "author");
                 int source = 0;
                 for (int j = 0; j < nodes.size(); j++) {
                 	if (nodes.get(j).get("title").equals(name)) {
                 		source = (int) nodes.get(j).get("id");
                 		break;
-                	} 
+                	}
                 }
                 if (source == 0) {
                 	author.put("id", i);
@@ -75,8 +74,8 @@ public class PaperService {
         result.put(key2,value2);
         return result;
     }
-    
-    private Map<String, Object> map3(String key1, Object value1, String key2, Object value2, 
+
+    private Map<String, Object> map3(String key1, Object value1, String key2, Object value2,
     		String key3, Object value3) {
         Map<String, Object> result = new HashMap<String,Object>(3);
         result.put(key1,value1);
@@ -84,8 +83,8 @@ public class PaperService {
         result.put(key3, value3);
         return result;
     }
-    
-    private Map<String, Object> map5(String key1, Object value1, String key2, Object value2, 
+
+    private Map<String, Object> map5(String key1, Object value1, String key2, Object value2,
     		String key3, Object value3, String key4, Object value4, String key5, Object value5) {
         Map<String, Object> result = new HashMap<String,Object>(5);
         result.put(key1,value1);
@@ -95,8 +94,8 @@ public class PaperService {
         result.put(key5, value5);
         return result;
     }
-    
-    private Map<String, Object> map6(String key1, Object value1, String key2, Object value2, 
+
+    private Map<String, Object> map6(String key1, Object value1, String key2, Object value2,
     		String key3, Object value3, String key4, Object value4, String key5, Object value5,
     		String key6, Object value6) {
         Map<String, Object> result = new HashMap<String,Object>(6);
@@ -113,7 +112,7 @@ public class PaperService {
         Iterator<Map<String, Object>> result = paperRepository.graph(limit).iterator();
         return toD3Format(result);
     }
-    
+
     public Map<String, Object> graphAlc(int limit) {
         Iterator<Map<String, Object>> result = paperRepository.graph(limit).iterator();
         return toAlcFormat(result);
