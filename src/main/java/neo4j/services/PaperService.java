2bb758e48f08f5eabacb214949d875066eb362f4
Chia-Chuan Wu <chia.chuan.wu@sv.cmu.edu>
2015-11-23 23:18:53 -0800

    change some q5 format

--- src/main/java/neo4j/services/PaperService.java
+++ src/main/java/neo4j/services/PaperService.java
@@ -12,16 +12,17 @@ import java.util.*;
 @Transactional
 public class PaperService {
 
-    @Autowired PaperRepository paperRepository;
+    @Autowired
+    PaperRepository paperRepository;
 
     private Map<String, Object> toD3Format(Iterator<Map<String, Object>> result) {
-        List<Map<String,Object>> nodes = new ArrayList<Map<String,Object>>();
-        List<Map<String,Object>> rels = new ArrayList<Map<String,Object>>();
+        List<Map<String, Object>> nodes = new ArrayList<Map<String, Object>>();
+        List<Map<String, Object>> rels = new ArrayList<Map<String, Object>>();
         int i = 0;
         int target = 0;
         while (result.hasNext()) {
             Map<String, Object> row = result.next();
-            nodes.add(map("name",row.get("paper"),"label","paper"));
+            nodes.add(map("name", row.get("paper"), "label", "paper"));
             i++;
             target = i;
             for (Object name : (Collection) row.get("cast")) {
@@ -38,8 +39,8 @@ public class PaperService {
     }
 
     private Map<String, Object> toAlcFormat(Iterator<Map<String, Object>> result) {
-        List<Map<String,Object>> nodes = new ArrayList<Map<String, Object>>();
-        List<Map<String,Object>> rels = new ArrayList<Map<String, Object>>();
+        List<Map<String, Object>> nodes = new ArrayList<Map<String, Object>>();
+        List<Map<String, Object>> rels = new ArrayList<Map<String, Object>>();
         int i = 1;
         int target = 0;
         while (result.hasNext()) {
@@ -50,13 +51,13 @@ public class PaperService {
                 Map<String, Object> author = map5("title", name, "label", name, "cluster", "2", "value", 1, "group", "author");
                 int source = 0;
                 for (int j = 0; j < nodes.size(); j++) {
-                	if (nodes.get(j).get("title").equals(name)) {
-                		source = (int) nodes.get(j).get("id");
-                		break;
-                	}
+                    if (nodes.get(j).get("title").equals(name)) {
+                        source = (int) nodes.get(j).get("id");
+                        break;
+                    }
                 }
                 if (source == 0) {
-                	author.put("id", i);
+                    author.put("id", i);
                     source = i;
                     i++;
                     nodes.add(author);
@@ -68,27 +69,64 @@ public class PaperService {
         return map("nodes", nodes, "edges", rels);
     }
 
+    private Map<String, Object> toQ5Format(Iterator<Map<String, Object>> result, String inputAuthor) {
+        List<Map<String, Object>> nodes = new ArrayList<Map<String, Object>>();
+        List<Map<String, Object>> rels = new ArrayList<Map<String, Object>>();
+        int i = 2;
+        int target = 0;
+
+        Map<String, Object> central = map5("title", inputAuthor, "label", inputAuthor, "cluster", "2", "value", 1, "group", "author");
+        central.put("id",1);
+        central.put("color", "red");
+        nodes.add(central);
+
+        while (result.hasNext()) {
+            Map<String, Object> row = result.next();
+            nodes.add(map6("id", i, "title", row.get("paper"), "label", row.get("paper"), "cluster", "1", "value", 2, "group", "paper"));
+            target = i++;
+            for (Object name : (Collection) row.get("cast")) {
+                Map<String, Object> author = map5("title", name, "label", name, "cluster", "2", "value", 1, "group", "author");
+                int source = 0;
+                for (int j = 0; j < nodes.size(); j++) {
+                    if (nodes.get(j).get("title").equals(name)) {
+                        source = (int) nodes.get(j).get("id");
+                        break;
+                    }
+                }
+                if (source == 0) {
+                    author.put("id", i);
+                    source = i;
+                    i++;
+                    nodes.add(author);
+                }
+                rels.add(map3("from", 1, "to", target, "title", "PUBLISH"));
+                rels.add(map3("from", source, "to", target, "title", "PUBLISH"));
+            }
+        }
+        return map("nodes", nodes, "edges", rels);
+    }
+
     private Map<String, Object> map(String key1, Object value1, String key2, Object value2) {
-        Map<String, Object> result = new HashMap<String,Object>(2);
-        result.put(key1,value1);
-        result.put(key2,value2);
+        Map<String, Object> result = new HashMap<String, Object>(2);
+        result.put(key1, value1);
+        result.put(key2, value2);
         return result;
     }
 
     private Map<String, Object> map3(String key1, Object value1, String key2, Object value2,
-    		String key3, Object value3) {
-        Map<String, Object> result = new HashMap<String,Object>(3);
-        result.put(key1,value1);
-        result.put(key2,value2);
+                                     String key3, Object value3) {
+        Map<String, Object> result = new HashMap<String, Object>(3);
+        result.put(key1, value1);
+        result.put(key2, value2);
         result.put(key3, value3);
         return result;
     }
 
     private Map<String, Object> map5(String key1, Object value1, String key2, Object value2,
-    		String key3, Object value3, String key4, Object value4, String key5, Object value5) {
-        Map<String, Object> result = new HashMap<String,Object>(5);
-        result.put(key1,value1);
-        result.put(key2,value2);
+                                     String key3, Object value3, String key4, Object value4, String key5, Object value5) {
+        Map<String, Object> result = new HashMap<String, Object>(5);
+        result.put(key1, value1);
+        result.put(key2, value2);
         result.put(key3, value3);
         result.put(key4, value4);
         result.put(key5, value5);
@@ -96,11 +134,11 @@ public class PaperService {
     }
 
     private Map<String, Object> map6(String key1, Object value1, String key2, Object value2,
-    		String key3, Object value3, String key4, Object value4, String key5, Object value5,
-    		String key6, Object value6) {
-        Map<String, Object> result = new HashMap<String,Object>(6);
-        result.put(key1,value1);
-        result.put(key2,value2);
+                                     String key3, Object value3, String key4, Object value4, String key5, Object value5,
+                                     String key6, Object value6) {
+        Map<String, Object> result = new HashMap<String, Object>(6);
+        result.put(key1, value1);
+        result.put(key2, value2);
         result.put(key3, value3);
         result.put(key4, value4);
         result.put(key5, value5);
@@ -120,7 +158,7 @@ public class PaperService {
 
     public Map<String, Object> q5(String name) {
         Iterator<Map<String, Object>> result = paperRepository.q5(name, 100).iterator();
-        return toAlcFormat(result);
+        return toQ5Format(result, name);
     }
 }
 
