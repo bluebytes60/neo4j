ce962fd4cd570173a440e4ac45f74780a8bbe198
Chia-Chuan Wu <chia.chuan.wu@sv.cmu.edu>
2015-12-01 21:35:07 -0800

    finished q12

--- src/main/java/neo4j/services/Q12.java
+++ src/main/java/neo4j/services/Q12.java
@@ -9,9 +9,7 @@ import util.MapUtil;
 import util.Rest;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by bluebyte60 on 12/1/15.
@@ -41,7 +39,7 @@ public class Q12 {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return result.subList(0, K);
+        return result.subList(0, K > result.size() ? result.size() : K);
     }
 
     public Map<String, Object> parse(String keyword, int K) {
@@ -49,17 +47,55 @@ public class Q12 {
         List<String> titles = topK(keyword, K);
         List<Map<String, Object>> nodes = new ArrayList<>();
         List<java.util.Map<String, Object>> rels = new ArrayList<>();
+        //prevent duplicate data
+        Set<String> ids = new HashSet<>();
+        //first step find top k paper and their co-author
         for (String title : titles) {
             Graph g = Rest.query(String.format(query, title));
+            System.out.println(String.format(query, title));
             for (Node node : g.getNodes()) {
-                if (node.getLabels().get(0).equals("Paper"))
-                    nodes.add(MapUtil.map5("id", node.getId(), "label", node.getProperties().getTitle(), "cluster", "1", "value", 2, "group", "paper"));
-                else
-                    nodes.add(MapUtil.map5("id", node.getId(), "label", node.getProperties().getName(), "cluster", "2", "value", 1, "group", "author"));
+                if (node.getLabels().get(0).equals("Paper")) {
+                    if (!ids.contains(node.getId())) {
+                        nodes.add(MapUtil.map6("id", node.getId(), "label", node.getProperties().getTitle(), "cluster", "1", "value", 2, "group", "paper", "color", "red"));
+                        ids.add(node.getId());
+                    }
+                } else {
+                    if (!ids.contains(node.getId())) {
+                        nodes.add(MapUtil.map5("id", node.getId(), "label", node.getProperties().getName(), "cluster", "2", "value", 1, "group", "author"));
+                        ids.add(node.getId());
+                    }
+                }
             }
 
             for (Relationship relationship : g.getRelationships()) {
                 rels.add(MapUtil.map3("from", relationship.getStartNode(), "to", relationship.getEndNode(), "title", "PUBLISH"));
+            }
+        }
+
+        //second step, find shortest path between these papers
+        //MATCH p = shortestPath((a1:Paper { title:"Fair sticker languages." })-[*1..15]-(a2:Paper { title:"On the Generative Power of Regular Pattern Grammars." })) RETURN p
+        for (int i = 0; i < titles.size(); i++) {
+            for (int j = i + 1; j < titles.size(); j++) {
+                query = "MATCH p = shortestPath((a1:Paper { title:\\\"%s\\\" })-[*1..5]-(a2:Paper { title:\\\"%s\\\" })) RETURN p";
+                System.out.println(String.format(query, titles.get(i), titles.get(j)));
+                Graph g = Rest.query(String.format(query, titles.get(i), titles.get(j)));
+                for (Node node : g.getNodes()) {
+                    if (node.getLabels().get(0).equals("Paper")) {
+                        if (!ids.contains(node.getId())) {
+                            nodes.add(MapUtil.map5("id", node.getId(), "label", node.getProperties().getTitle(), "cluster", "1", "value", 2, "group", "paper"));
+                            ids.add(node.getId());
+                        }
+                    } else {
+                        if (!ids.contains(node.getId())) {
+                            nodes.add(MapUtil.map5("id", node.getId(), "label", node.getProperties().getName(), "cluster", "2", "value", 1, "group", "author"));
+                            ids.add(node.getId());
+                        }
+                    }
+                }
+
+                for (Relationship relationship : g.getRelationships()) {
+                    rels.add(MapUtil.map3("from", relationship.getStartNode(), "to", relationship.getEndNode(), "title", "PUBLISH"));
+                }
             }
         }
         return MapUtil.map("nodes", nodes, "edges", rels);
